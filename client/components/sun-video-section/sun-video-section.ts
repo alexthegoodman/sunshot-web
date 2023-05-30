515800e681983fcd0dfb80c71572cb9c20414f3c
Alex Goodman <alexthegoodman@gmail.com>
2023-05-30 19:20:05 -0400

    video section

--- client/components/sun-video-section/sun-video-section.ts
+++ client/components/sun-video-section/sun-video-section.ts
@@ -1,12 +1,38 @@
 import {LitElement, html, unsafeCSS} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 import styles from './sun-video-section.scss?inline';
 
 @customElement('sun-video-section')
 export class SunVideoSection extends LitElement {
   static override styles = unsafeCSS(styles);
 
+  @property({type: String})
+  videoSrc = '';
+
   override render() {
-    return html`<div></div>`;
+    return html`
+      <section class="videoSection">
+        <div class="videoSectionInner">
+          <div class="video">
+            <video autoplay muted loop playsinline>
+              <source src=${this.videoSrc} type="video/mp4" />
+            </video>
+          </div>
+          <!-- video -->
+          <div class="content">
+            <div class="icon">
+              <slot name="icon"></slot>
+            </div>
+            <div class="title">
+              <slot name="title"></slot>
+            </div>
+            <div class="description">
+              <slot name="description"></slot>
+            </div>
+          </div>
+          <!-- content -->
+        </div>
+      </section>
+    `;
   }
 }
