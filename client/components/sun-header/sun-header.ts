71b667a8ab9f59b7b10591455b05c5aae417c3c1
Alex Goodman <alexthegoodman@gmail.com>
2023-08-29 19:26:01 -0400

    more responsive work

--- client/components/sun-header/sun-header.ts
+++ client/components/sun-header/sun-header.ts
@@ -1,17 +1,20 @@
 import {LitElement, html, unsafeCSS} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, state} from 'lit/decorators.js';
 import styles from './sun-header.scss?inline';
 
 @customElement('sun-header')
 export class SunHeader extends LitElement {
   static override styles = unsafeCSS(styles);
 
+  @state()
+  _menuOpen = false;
+
   override render() {
     return html`
       <header>
         <div class="headerInner">
           <div class="logoWrapper"><slot name="logo"></slot></div>
-          <div class="headerCtrls">
+          <div class="headerCtrls ${this._menuOpen ? 'open' : ''}">
             <nav>
               <ul>
                 <li>
@@ -22,13 +25,21 @@ export class SunHeader extends LitElement {
             </nav>
             <button class="btn">Download for Windows</button>
           </div>
+          <div class="mobile">
+            <img src="/images/menu.svg" @click=${this._openMenu} />
+          </div>
         </div>
       </header>
     `;
   }
 
+  _openMenu() {
+    this._menuOpen = !this._menuOpen;
+  }
+
   // scroll to #pricing on click
   private scrollToPricing() {
+    this._openMenu();
     const pricingEl = document.getElementById('pricing');
     console.info('pricingEl', pricingEl);
     pricingEl?.scrollIntoView({behavior: 'smooth'});
