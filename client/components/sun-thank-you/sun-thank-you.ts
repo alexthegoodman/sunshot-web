f263bb14118579378b09521a14bbb47a820560c3
Alex Goodman <alexthegoodman@gmail.com>
2023-09-15 21:44:59 -0400

    basic thank you page

---
+++ client/components/sun-thank-you/sun-thank-you.ts
@@ -0,0 +1,21 @@
+import {LitElement, html, unsafeCSS} from 'lit';
+import {customElement} from 'lit/decorators.js';
+import styles from './sun-thank-you.scss?inline';
+
+@customElement('sun-thank-you')
+export class ThankYou extends LitElement {
+  static override styles = unsafeCSS(styles);
+
+  override render() {
+    return html`
+      <div class="thankYou">
+        <h1>Thank you!</h1>
+        <p>
+          Hopefully, you are completely satisfied with your purchase. Feedback
+          is always welcome. Support is available at
+          <a href="mailto:admin@sunshot.app">admin@sunshot.app</a>
+        </p>
+      </div>
+    `;
+  }
+}
